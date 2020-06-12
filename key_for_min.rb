297a5a1ee2d75f06f59c21fe405a43c245b9ac78
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-12 20:54:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(family_ages)
-  key_for_min_value = {
+family_ages = {
     :Tiva => 15,
     :Mareva => 2,
     :Edith => 42,
     :Muzo => 41,
     :Naya => 21
   }
+
+def key_for_min_value(family_ages)
+
 
 end
 
