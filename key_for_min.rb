3f03429bb239267f42215ca8eabb78365b4ab0bc
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-12 20:51:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  key_for_min_value = {
+    :Tiva => 15,
+    :Mareva => 2,
+    :Edith => 42,
+    :Muzo => 41,
+    :Naya => 21
+  }
 
 end
